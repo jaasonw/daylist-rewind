594a79962a5f5281bb46a33deb35a02b2460202f
jason wong <jayywong92@gmail.com>
2024-07-27 13:03:12 -0700

    fix docker pt 3

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -1,3 +1,4 @@
+export const dynamic = 'force-dynamic';
 import { ThemeProvider } from "@/components/ThemeProvider";
 import {
   Home,
--- src/app/register/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { AuthProvider } from "@/components/PocketBaseAuthProvider";
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return <AuthProvider>{children}</AuthProvider>;
-}
